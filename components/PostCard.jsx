297f546a227a20c2761e7817773731887c2259ea
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-07-04 16:58:39 +0200

    resolved: tsconfig issues and missing qpcms fields

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const PostCard = ({post}) => {
+const PostCard = ({ post }) => {
+  console.log(`${post}`);
   return (
-    <div>
-        {post.title}
-        {post.excerpt}
+    <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+      <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+        {/* <img src="" alt="" /> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
